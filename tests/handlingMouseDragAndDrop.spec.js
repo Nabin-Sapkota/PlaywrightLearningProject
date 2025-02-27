38a9323a3f05993e0f9be8916eeb5c3f7bd6cfec
nabin sapkota <sapkotanabin383@gmail.com>
2025-02-27 20:58:13 +0545

    New updates

--- tests/handlingMouseDragAndDrop.spec.js
+++ tests/handlingMouseDragAndDrop.spec.js
@@ -1,5 +1,16 @@
 import {test, expect} from '@playwright/test'
 
-test("Drag and Drop", ({page}) => {
+test("Drag and Drop", async({page}) => {
+
+    // await page.goto("URL");
+    // const sourceElement = page.locator("Locate the element to drop here");
+    // const targetElement = page.locator("locate the place to drop here");
+
+    // await sourceElement.dragAndDrop(targetElement);
+
+    // const droppedElement = targetElement.locator("locate the element after dropping");
+    // await expect(droppedElement).toBeVisible
+
+
     
 })
